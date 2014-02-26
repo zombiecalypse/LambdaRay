b7fd205873c50b226926f29b829a8069293e2386
Aaron Karper <maergil@gmail.com>
2014-02-26 14:43:18 +0100

    Hardcore optimization
    0.5x run time. Currently the Film eats ~50% of the run time.

--- src/LambdaRay/Film.hs
+++ src/LambdaRay/Film.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
 module LambdaRay.Film where
 import LambdaRay.Helper
 import LambdaRay.Types
@@ -5,23 +6,33 @@ import Control.Monad (forM_)
 import Control.Monad.ST.Safe
 import qualified Codec.Picture as CI
 import qualified Data.Array.ST as A
-import Data.Array
+import Data.Array.Unboxed
 
-type ArrayT s = (A.STArray s (Int, Int) (Int, Spectrum))
-avg :: Int -> Int -> [Sample] -> Array (Int,Int) (Int, Spectrum)
-avg w h samples = A.runSTArray (do
-      array <- A.newArray ((0,0), (w,h)) (0, nullSpectrum) :: ST s (ArrayT s)
-      forM_ samples (update array)
+import qualified Codec.Picture as CI
+
+foreign import ccall unsafe "math.h floor"
+    c_floor :: Double -> Int
+
+tr = c_floor
+
+avg :: Int -> Int -> [Sample] -> UArray (Int,Int, Int) Float
+avg w h samples = A.runSTUArray (do
+      array <- A.newArray ((0,0,0), (w,h,2)) 0:: ST s (A.STUArray s (Int, Int, Int) Float)
+      counts <- A.newArray ((0,0,0), (w,h,2)) 0:: ST s (A.STUArray s (Int, Int, Int) Int)
+      forM_ samples' (update array counts)
       return array)
   where
-    update array (Sample x y v) = do
-      let x'=floor x
-      let y'=floor y
-      (count, spectrum) <- A.readArray array (x', y')
-      let count' = count + 1 :: Int
-      let frac = (fromIntegral count :: Float) / fromIntegral count'
-      let spectrum' = ((frac `scaleSpectrum` spectrum) + v)
-      A.writeArray array (x',y') (count', spectrum')
+    samples' = pmap (\(Sample x y (CI.PixelRGBF r g b)) -> (tr x, tr y, r, g, b)) samples
+    update array counts (x', y', r, g, b) = do
+      let smpl = [r, g, b]
+      forM_ [0,1,2] (\c -> do
+        count <- A.readArray counts (x', y', c)
+        chan <- A.readArray array (x', y', c)
+        let count' = count + 1 :: Int
+        let frac = (fromIntegral count :: Float) / fromIntegral count'
+        let chan' = frac * chan + (1-frac) * (smpl !! c)
+        A.writeArray counts (x',y', c) count'
+        A.writeArray array (x',y', c) chan')
 
 boxFilterFilm :: Film
 boxFilterFilm scene samples = CI.generateImage (develop samples) w h
@@ -29,4 +40,6 @@ boxFilterFilm scene samples = CI.generateImage (develop samples) w h
     w = fromInteger $ width scene :: Int
     h = fromInteger $ height scene :: Int
     develop :: [Sample] -> Int -> Int -> CI.PixelRGBF
-    develop samples x y = snd $ avg w h samples ! (x,y)
+    develop samples x y = spec x y
+    averg = avg w h samples
+    spec x y = CI.PixelRGBF (averg ! (x,y,0)) (averg ! (x,y,1)) (averg ! (x,y,2))
